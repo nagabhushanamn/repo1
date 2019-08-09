2c19807271addb7ca1ee1190a69184b223023d38
Nag-Training <nag-training@NAGABHUSHANAMs-MacBook-Pro.local>
2019-08-09 23:46:52 +0530

    dir change

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'app1' });
+  res.render('index', { title: 'DevOps' });
 });
 
 module.exports = router;
